02fe28d0de66a7b3551ea99337bc842cd952cce2
RuslanRad <r140390@yandex.ru>
2024-11-11 23:03:19 +0500

    1.7. Конструктор [8]

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+
+    }
+
+    public void printInfo() {
+        System.out.println("Активность : " + active);
+        System.out.println("Статус : " + status);
+        System.out.println("Сообщение : " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error(true, 1, "Ошибка №1");
+        Error error2 = new Error(false, 0, "Ошибка №2");
+        Error error3 = new Error(true, 2, "Ошибка №3");
+
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+
+}
