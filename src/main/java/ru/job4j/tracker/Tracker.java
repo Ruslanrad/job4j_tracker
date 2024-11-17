c0eceb68014bbb52220d1db7317c30a1667be932
RuslanRad <r140390@yandex.ru>
2024-11-17 13:56:35 +0500

    5. Tracker - хранилище [38]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -20,6 +22,32 @@ public class Tracker {
                 break;
             }
         }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int sizeRsl = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index] != null) {
+                rsl[sizeRsl] = items[index];
+                sizeRsl++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, sizeRsl);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int sizeRsl = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getName().equals(key)) {
+                rsl[sizeRsl] = items[index];
+                sizeRsl++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, sizeRsl);
         return rsl;
     }
 }
