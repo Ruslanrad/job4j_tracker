e30a1405ffd567f4eb9d64fe0ed250c3717699cb
RuslanRad <r140390@yandex.ru>
2024-12-03 18:03:49 +0500

    2.1. Тест на ValidateInput [101]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.MockInput;
+import ru.job4j.tracker.input.ValidateInput;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenInvalidInputInputThree() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"3"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(3);
+    }
+
+    @Test
+    void whenInvalidInputInputOneTwoThree() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(2);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(3);
+    }
+
+    @Test
+    void whenInvalidInputInputMinusFive() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"-5"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-5);
+    }
+}
